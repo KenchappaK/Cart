f1915c51d64c3d15b9ffa92115b66e85cfd2f17f
KenchappaK <kenchappakattikar@gmail.com>
2023-10-22 22:33:15 +0530

    added cart

---
+++ src/utils.js
@@ -0,0 +1,11 @@
+ const getTotals = (cart) =>{
+    // console.log(cart)
+    let totalAmount =0;
+    let totalCost =0;
+    for(let {amount, price} of cart.values()){
+        totalAmount += amount;
+        totalCost += amount*price;
+    }
+    return{totalAmount, totalCost};
+}
+export default getTotals;
